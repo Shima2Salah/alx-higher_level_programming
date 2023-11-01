6baa665723d57b7eddb3d0faeb8ac9fd510d6b7f
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-01 15:54:46 -0700

    Python

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    """magic calculation solution part 2"""
-    if a < b:
+    if (a < b):
         return (c)
+    elif (c > b):
+        return (a + b)
     else:
-        if c > b:
-            return (a + b)
-    return (a * b - c)
+        return (a * b - c)

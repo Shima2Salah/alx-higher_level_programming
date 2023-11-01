12d3da9c8c92edd7de2b51921dced5bef65beb78
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-01 14:26:22 -0700

    Python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
+for n in range(121, 96, -2):
+    print("{:c}{:c}".format(n + 1, n - 32), end="")

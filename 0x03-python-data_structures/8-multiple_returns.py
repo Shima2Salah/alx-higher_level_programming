5fb54dfbfb51b2e9ed826c7e8bdebee96414995f
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-04 03:38:10 -0700

    Python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     a = len(sentence) + 0
-    if (sentence[0] is None):
+    if (sentence == ""):
         b = None
     else:
         b = sentence[0]

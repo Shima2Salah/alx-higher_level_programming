79052284f51d7392e7e4d636a6c41faf6563116a
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-08 13:18:40 -0800

    Python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max = 0
+    max = None
     for x, y in a_dictionary.items():
-        if max <= y:
+        if max is None or max < y:
             max = y
             max_key = x
     return max_key

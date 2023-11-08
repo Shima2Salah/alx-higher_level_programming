68b709df64973f979e98100c27368d31b91648a7
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-08 09:42:34 -0800

    Python

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    n = 0
-    for i in range(len(my_list)):
-        n += my_list[i]
-        for j in range(i + 1, len(my_list)):
-            if my_list[i] == my_list[j]:
-                n -= my_list[j]
-    return (n)
+    n = set(my_list)
+    return (sum(n))

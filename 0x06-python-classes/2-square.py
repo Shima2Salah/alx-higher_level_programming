ceeea5b33c4d0f6b9fd848f093b1fcf6b4dbbf01
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-21 14:10:14 -0800

    Python

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A module that declare a class"""
+
+
+class Square:
+    """It is an empty class called square"""
+    def __init__(self, size=0):
+        """
+        intialize method for function
+        Args:
+        size value
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

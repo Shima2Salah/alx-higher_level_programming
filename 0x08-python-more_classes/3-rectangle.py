9825939302ce27df13d5ec35f7e0d8d6cbccf4ae
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-27 11:10:46 -0800

    Python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,9 +57,9 @@ class Rectangle:
         if (self.__width == 0 or self.__height == 0):
             return 0
         return (2 * (self.__width + self.__height))
-    
+
     def __str__(self):
-        ddd"""print # as rectangle area"""
+        """print # as rectangle area"""
         i = 0
         a = ""
         if (self.__width == 0 or self.__height == 0):

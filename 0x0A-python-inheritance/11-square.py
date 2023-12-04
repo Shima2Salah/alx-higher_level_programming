0d14ca25cf91ad0a6b6c06eddaa663a6cb6e5158
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-12-04 15:17:00 -0800

    Python

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module that return is object or not"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """class square child"""
+
+    def __init__(self, size):
+        """int func validator function"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """print string"""
+        return (f"[Square] {self.__size}/{self.__size}")

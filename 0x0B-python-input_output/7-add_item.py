d59b570050cd217df222203a464317f1094057cd
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-12-05 14:27:41 -0800

    Python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Script that stores args in json"""
 import sys
+import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 
 filename = "add_item.json"
 sn = sys.argv[0]
 args = sys.argv[1:]
-save_to_json_file(sys.argv[1:], filename)
-load_from_json_file(filename)
+existing_data = load_from_json_file(filename)
+save_to_json_file(existing_data + args, filename)

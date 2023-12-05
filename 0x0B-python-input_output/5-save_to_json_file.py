529607c51af7a47e653d64f54e6f455c81e44593
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-12-05 13:26:45 -0800

    Python

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,6 +10,5 @@ def save_to_json_file(my_obj, filename):
         my_obj: entered object
         filename: json file
     """
-    js_str = json.dumps(my_obj)
     with open(filename, 'w') as f:
-        f.write(js_str)
+        json.dump(my_obj, f)

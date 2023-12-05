00d7175c14e4c229a1d0a4c455cb76ded859fd0b
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-12-05 12:05:56 -0800

    Python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """read file function"""
     with open(filename, 'r', encoding='utf-8')as file:
-        f = file.read()
-    print(f)
+        for line in file:
+            print(line.strip())

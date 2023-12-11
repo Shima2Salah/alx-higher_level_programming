e3e86af4caf4d49f82538834b83212b82d788c8d
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-12-12 01:27:30 +0200

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -59,23 +59,6 @@ class TestBaseInstatiation(unittest.TestCase):
     def test_two_args(self):
         with self.assertRaises(TypeError):
             Base(1, 2)
-    
-    def test_init(self):
-        """testing for base class initialize"""
-        obj1 = Base()
-        self.assertEqual(obj1.id, 1)
-
-        obj2 = Base()
-        self.assertEqual(obj2.id, 2)
-
-        obj3 = Base(15)
-        self.assertEqual(obj3.id, 15)
-
-        obj4 = Base()
-        self.assertEqual(obj4.id, 3)
-
-        obj5 = Base(None)
-        self.assertEqual(obj5.id, 4)
 
 if __name__ == "__main__":
     unittest.main()

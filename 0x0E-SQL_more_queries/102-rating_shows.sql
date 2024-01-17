cb842c087e54c1faaecbb83c741ca2c7c283a593
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-01-17 15:27:42 -0800

    mysql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,4 @@
--- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_show_ratings
-ON tv_show_genres.show_id = tv_show_ratings.show_id
+-- script that lists all shows from hbtn_0d_tvshows_rate by their ratingd
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
 ORDER BY rating DESC;

31f8528bcce33d595cdc1bec54158ec568cc12d0
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-01-17 14:17:32 -0800

    mysql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,4 @@ SELECT tv_genres.name FROM tv_genres JOIN tv_show_genres JOIN tv_shows
 ON tv_genres.id = tv_show_genres.genre_id
 and tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name DESC;
+ORDER BY tv_genres.name;

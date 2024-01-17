0ef5e76d82c520e9ad54a3ef20040fec96a0815c
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-01-17 12:33:37 -0800

    mysql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- script that lists all the cities of California in hbtn_0d_usa.
-SELECT id, name FROM states.cities 
-WHERE  name = California ORDER BY cities.id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE  name = 'California') ORDER BY id;
+

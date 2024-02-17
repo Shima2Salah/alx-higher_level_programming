0de71863d397a7d207bb0dbb811dd1f6a22c865f
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-02-17 09:48:29 -0800

    mysql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
                            user=sys.argv[1],
                            passwd=sys.argv[2],
                            db=sys.argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

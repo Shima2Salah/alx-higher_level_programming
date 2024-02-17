efe971feeed4adbfd87145b7787b361a653cb860
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-17 22:54:19 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""To List all states from database hbtn_0e_0_usa"""
-import sys
+# Takes in the name of a state as argument and lists all cities of that state.
+# And it is safe of SQL injection.
 import MySQLdb
+import sys
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3])
-    cur = conn.cursor()
-    sql = "SELECT cities.name FROM cities INNER JOIN states ON\
-           cities.state_id = states.id\
-           WHERE states.name = %s ORDER BY cities.id ASC "
-    data = (sys.argv[4], )
-    cur.execute(sql, data)
-    query_rows = cur.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id\
+                = states.id WHERE states.name LIKE BINARY %s ORDER BY\
+                cities.id;", (sys.argv[4], ))
+    result = cur.fetchall()
     l = []
-    for row in query_rows:
+    for row in result:
         l.append(row[0])
     print(", ".join(l))
     cur.close()
-    conn.close()
+    db.close()

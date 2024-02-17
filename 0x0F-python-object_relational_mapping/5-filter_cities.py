8d3ab7d65004a6ef3c03dcf63ca251f3dc641367
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-02-17 12:00:43 -0800

    mysql

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""To List all states from database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    sql = "SELECT cities.name FROM cities, states WHERE cities.state_id\
+           = states.id and states.name = %s ORDER BY cities.id ASC"
+    data = (sys.argv[4], )
+    cur.execute(sql, data)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

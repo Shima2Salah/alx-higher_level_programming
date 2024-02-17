68d937684863716d21c828120330ca289c0a80c7
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-02-17 14:22:20 -0800

    mysql

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,16 +3,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True,
                 unique=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-.format("root", "root", "hbtn_0e_6_usa"), pool_pre_ping=True)
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root",
+                       "root", "hbtn_0e_6_usa"), pool_pre_ping=True)
 Base.metadata.create_all(engine)
-Session = sessionmaker(bind = engine)
+Session = sessionmaker(bind=engine)
 session = Session()
 for state in session.query(State).order_by(State.id).all():
     print("{}: {}".format(state.id, state.name))

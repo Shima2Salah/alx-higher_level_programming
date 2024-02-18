f5008061d91d24087272fa4486a8da338e49fc17
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-19 01:04:46 +0200

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""create table contain states"""
-from model_state import Base
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+
+"""
+    this module contains a Base and City class
+"""
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
 
 
 Base = declarative_base()
 
 
 class City(Base):
-    """class contains states information"""
+    """
+        City class inherits the Base class
+        Attributes:
+            id (int)
+            name (string)
+            state_id (string)
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False)
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, Foreignkey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

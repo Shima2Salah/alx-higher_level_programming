5476048880c03a1d752984edac6c6a5ec814d0a9
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-02-29 14:08:02 -0800

    python

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Finds a peak in a listusing binary search."""
 
+
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted int"""
     li = list_of_integers

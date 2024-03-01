3fa9cd1e1afca80b304a4c917a3d66ac437d3766
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-03-01 11:09:43 -0800

    python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 from urllib import request
 
 if __name__ == "__main__":
-        with request.urlopen("https://alx-intranet.hbtn.io/status") as response
-                content = response.read()
-                print("Body response:")
-                print("- type: {}".format(type(content)))
-                print("- content: b{}".format(content.status_code))
-                print("- utf8 content: {}".format(content.decode("utf-8").status_code)):
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        content = response.read()
+        print("Body response:")
+        print("- type: {}".format(type(content)))
+        print("- content: {}".format(content))
+        print("- utf8 content: {}".format(content.decode("utf-8")))

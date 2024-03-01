bd245b91177c68093cbe64d1c20050a441555e86
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-01 20:56:59 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
-
-n script that fetches url"""
+#!/usr/bin/python3
+"""Python script that fetches url"""
 from urllib import request
-import sys
+
 if __name__ == "__main__":
         with request.urlopen("https://alx-intranet.hbtn.io/status") as response
                 content = response.read()
-                        print("Body response:")
-                                print("- type: {}".format(type(content)))
-                                        print("- content: b{}".format(content.status_code))
-                                                print("- utf8 content: {}".format(content.status_code.decode("utf-8"))):
+                print("Body response:")
+                print("- type: {}".format(type(content)))
+                print("- content: b{}".format(content.status_code))
+                print("- utf8 content: {}".format(content.decode("utf-8").status_code)):

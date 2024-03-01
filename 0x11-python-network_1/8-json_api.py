4fb94612edc5463565fe7a7f5523bc527a0328db
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-02 01:06:30 +0200

    python

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that fetches url"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if sys.argv[1]:
+        q = sys.argv[1]
+    else:
+        q=""
+    response = requests.get(url, params={'q':q})
+    try:
+        r = response.json()
+    except:
+        print("Not a valid JSON")
+    else:
+        if q == "":
+            print("No result")
+        else:
+            print("[{}] {}".format(r[id], r[name]))

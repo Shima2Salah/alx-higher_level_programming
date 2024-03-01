cf018f1aaf1ffeb1c79a1f1a61e7c5aba6ac8a8f
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-01 21:14:02 +0200

    python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("- type: {}".format(type(content)))
         print("- content: {}".format(content))
-        print("- utf8 content: {}".format(content.decode("utf-8")))
+        print("- utf8 content: {}".format(content.decode("utf8")))
